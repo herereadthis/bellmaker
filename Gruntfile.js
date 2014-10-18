39c755a7dfcc3c9d27e01be3181599abf1339db0
herereadthis <herereadthis@gmail.com>
2014-10-17 22:39:43 -0400

    #17 massive edits make LESS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,107 +1,422 @@
+// Generated on 2014-10-02 using
+// generator-webapp 0.5.0
+'use strict';
 
-// The "wrapper" function
-module.exports = function(grunt) {
-    // Do grunt-related things in here
-    // Project configuration.
-    require('load-grunt-tasks')(grunt);
-    grunt.initConfig({
-        // imports the JSON metadata stored in package.json
-        pkg: grunt.file.readJSON('package.json'),
-        // compiles LESS file to minified CSS
-        jekyll: {
-            dev: {
-                src: 'templates',
-                dest: 'dev'
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// If you want to recursively match all subfolders, use:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+    // Configurable paths
+    var config = {
+        src: 'src',
+        app: 'app',
+        dist: 'dist',
+        tmp: '.tmp'
+    };
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+
+        // Project settings
+        config: config,
+
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            bower: {
+                files: ['bower.json'],
+                tasks: ['wiredep']
+            },
+            js: {
+                files: ['<%= config.app %>/scripts/{,*/}*.js'],
+                tasks: ['jshint'],
+                options: {
+                    livereload: true
+                }
+            },
+            jstest: {
+                files: ['test/spec/{,*/}*.js'],
+                tasks: ['test:watch']
+            },
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+            less: {
+                files: [
+                    '<%= config.app %>/less/{,*/}*.less',
+                    '<%= config.src %>/less/{,*/}*.less'
+                ],
+                tasks: ['less']
+            },
+            styles: {
+                files: ['<%= config.app %>/styles/{,*/}*.css'],
+                tasks: ['newer:copy:styles', 'autoprefixer']
+            },
+            livereload: {
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= config.app %>/{,*/}*.html',
+                    '.tmp/styles/{,*/}*.css',
+                    '<%= config.app %>/images/{,*/}*'
+                ]
             }
         },
+
+    // The actual grunt server settings
+    connect: {
+      options: {
+        port: 9000,
+        open: true,
+        livereload: 35729,
+        // Change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function(connect) {
+            return [
+              connect.static('.tmp'),
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect.static(config.app)
+            ];
+          }
+        }
+      },
+      test: {
+        options: {
+          open: false,
+          port: 9001,
+          middleware: function(connect) {
+            return [
+              connect.static('.tmp'),
+              connect.static('test'),
+              connect().use('/bower_components', connect.static('./bower_components')),
+              connect.static(config.app)
+            ];
+          }
+        }
+      },
+      dist: {
+        options: {
+          base: '<%= config.dist %>',
+          livereload: false
+        }
+      }
+    },
+
+    // Empties folders to start fresh
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp',
+            '<%= config.dist %>/*',
+            '!<%= config.dist %>/.git*'
+          ]
+        }]
+      },
+      server: '.tmp'
+    },
+
+    // Make sure code styles are up to par and there are no obvious mistakes
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        'Gruntfile.js',
+        '<%= config.app %>/scripts/{,*/}*.js',
+        '!<%= config.app %>/scripts/vendor/*',
+        'test/spec/{,*/}*.js'
+      ]
+    },
+
+    // Mocha testing framework configuration options
+    mocha: {
+      all: {
+        options: {
+          run: true,
+          urls: ['http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html']
+        }
+      }
+    },
+
+    // Add vendor prefixed styles
+    autoprefixer: {
+      options: {
+        browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1']
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/styles/',
+          src: '{,*/}*.css',
+          dest: '.tmp/styles/'
+        }]
+      }
+    },
         less: {
+            bellmaker: {
+                options: {
+                },
+                files: {
+                    '<%= config.tmp %>/styles/main.css': '<%= config.app %>/less/imports.less'
+                }
+            },
             minifiedLess: {
                 options: {
-                    paths: ["./src/less", "./src/demo/less"],
                     cleancss: true
                 },
                 files: {
-                    "./src/css/main.less-minified.css": "./src/less/bellmaker.less"
+                    "<%= config.dist %>/css/main.less-minified.css": "<%= config.src %>/less/bellmaker.less"
                 }
             },
             uncompressedLess: {
                 options: {
-                    paths: ["./src/less", "./src/demo/less"],
                     cleancss: false
                 },
                 files: {
-                    "./src/css/main.less.css": "./src/less/bellmaker.less"
+                    "<%= config.dist %>/css/main.less.css": "<%= config.src %>/less/bellmaker.less"
                 }
             },
             basicDemoLess: {
                 options: {
-                    paths: ["./src/less", "./src/demo/basic_demo/less"],
                     cleancss: false
                 },
                 files: {
-                    "./src/demo/basic_demo/css/main.css": "./src/demo/basic_demo/less/imports.less"
-                }  
-            },
-            bootstrapDemoLess: {
-                options: {
-                    paths: ["./src/less", "./src/demo/bootstrap_demo/less"],
-                    cleancss: true
-                },
-                files: {
-                    "./src/demo/bootstrap_demo/css/main.css": "./src/demo/bootstrap_demo/less/imports.less"
+                    "<%= config.dist %>/css/main.demo.css": '<%= config.app %>/less/imports.less'
                 }  
             }
         },
-        sass: {
-            minifiedSass: {
-                options: {
-                    style: 'compressed',
-                    precision: 4
-                },
-                files: {
-                    "./src/css/main.sass-minified.css": "./src/sass/main.scss"
-                }
-            },
-            uncompressedSass: {
-                options: {
-                    style: 'expanded',
-                    precision: 4
-                },
-                files: {
-                    "./src/css/main.sass.css": "./src/sass/main.scss"
-                }
-            }
-        },
-        watch: {
-            // runs less task when any less files change
-            less: {
-                files: ["./src/less/*", "./src/demo/*/less/*"],
-                tasks: ["less"]
-            },
-            html: {
-                files: "**/*.html"
-            },
-            sass: {
-                files: ["./src/sass/*", "./src/demo/sass/*"],
-                tasks: ['sass']  
-            }
-        }
-    });
 
-    grunt.registerTask('makeJekyll', [
-        // 'uglify'
-        'jekyll'
+    // Automatically inject Bower components into the HTML file
+    wiredep: {
+      app: {
+        ignorePath: /^\/|\.\.\//,
+        src: ['<%= config.app %>/index.html']
+      }
+    },
+
+    // Renames files for browser caching purposes
+    rev: {
+      dist: {
+        files: {
+          src: [
+            '<%= config.dist %>/scripts/{,*/}*.js',
+            '<%= config.dist %>/styles/{,*/}*.css',
+            '<%= config.dist %>/images/{,*/}*.*',
+            '<%= config.dist %>/styles/fonts/{,*/}*.*',
+            '<%= config.dist %>/*.{ico,png}'
+          ]
+        }
+      }
+    },
+
+    // Reads HTML for usemin blocks to enable smart builds that automatically
+    // concat, minify and revision files. Creates configurations in memory so
+    // additional tasks can operate on them
+    useminPrepare: {
+      options: {
+        dest: '<%= config.dist %>'
+      },
+      html: '<%= config.app %>/index.html'
+    },
+
+    // Performs rewrites based on rev and the useminPrepare configuration
+    usemin: {
+      options: {
+        assetsDirs: ['<%= config.dist %>', '<%= config.dist %>/images']
+      },
+      html: ['<%= config.dist %>/{,*/}*.html'],
+      css: ['<%= config.dist %>/styles/{,*/}*.css']
+    },
+
+    // The following *-min tasks produce minified files in the dist folder
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/images',
+          src: '{,*/}*.{gif,jpeg,jpg,png}',
+          dest: '<%= config.dist %>/images'
+        }]
+      }
+    },
+
+    svgmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= config.app %>/images',
+          src: '{,*/}*.svg',
+          dest: '<%= config.dist %>/images'
+        }]
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          conservativeCollapse: true,
+          removeAttributeQuotes: true,
+          removeCommentsFromCDATA: true,
+          removeEmptyAttributes: true,
+          removeOptionalTags: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true
+        },
+        files: [{
+          expand: true,
+          cwd: '<%= config.dist %>',
+          src: '{,*/}*.html',
+          dest: '<%= config.dist %>'
+        }]
+      }
+    },
+
+    // By default, your `index.html`'s <!-- Usemin block --> will take care
+    // of minification. These next options are pre-configured if you do not
+    // wish to use the Usemin blocks.
+    // cssmin: {
+    //   dist: {
+    //     files: {
+    //       '<%= config.dist %>/styles/main.css': [
+    //         '.tmp/styles/{,*/}*.css',
+    //         '<%= config.app %>/styles/{,*/}*.css'
+    //       ]
+    //     }
+    //   }
+    // },
+    // uglify: {
+    //   dist: {
+    //     files: {
+    //       '<%= config.dist %>/scripts/scripts.js': [
+    //         '<%= config.dist %>/scripts/scripts.js'
+    //       ]
+    //     }
+    //   }
+    // },
+    // concat: {
+    //   dist: {}
+    // },
+
+    // Copies remaining files to places other tasks can use
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= config.app %>',
+          dest: '<%= config.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            'images/{,*/}*.webp',
+            '{,*/}*.html',
+            'styles/fonts/{,*/}*.*'
+          ]
+        }, {
+          src: 'node_modules/apache-server-configs/dist/.htaccess',
+          dest: '<%= config.dist %>/.htaccess'
+        }]
+      },
+      styles: {
+        expand: true,
+        dot: true,
+        cwd: '<%= config.app %>/styles',
+        dest: '.tmp/styles/',
+        src: '{,*/}*.css'
+      }
+    },
+
+    // Run some tasks in parallel to speed up build process
+    concurrent: {
+      server: [
+        'copy:styles'
+      ],
+      test: [
+        'copy:styles'
+      ],
+      dist: [
+        'copy:styles',
+        'imagemin',
+        'svgmin'
+      ]
+    }
+  });
+
+
+  grunt.registerTask('serve', 'start the server and preview your app, --allow-remote for remote access', function (target) {
+    if (grunt.option('allow-remote')) {
+      grunt.config.set('connect.options.hostname', '0.0.0.0');
+    }
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'wiredep',
+      'concurrent:server',
+      'autoprefixer',
+      'connect:livereload',
+      'watch'
     ]);
-    grunt.registerTask('default', [
-        'less:minifiedLess',
-        'less:uncompressedLess',
-        'less',
-        'sass'
+  });
+
+  grunt.registerTask('server', function (target) {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run([target ? ('serve:' + target) : 'serve']);
+  });
+
+  grunt.registerTask('test', function (target) {
+    if (target !== 'watch') {
+      grunt.task.run([
+        'clean:server',
+        'concurrent:test',
+        'autoprefixer'
+      ]);
+    }
+
+    grunt.task.run([
+      'connect:test',
+      'mocha'
     ]);
-    grunt.registerTask('server', [
-        'watch'
-    ]);
-    grunt.registerTask('test', [
-        'less'
-    ]);
+  });
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'wiredep',
+    'useminPrepare',
+    'concurrent:dist',
+    'autoprefixer',
+    'less',
+    'concat',
+    'cssmin',
+    'uglify',
+    'copy:dist',
+    'rev',
+    'usemin',
+    'htmlmin'
+  ]);
+
+  grunt.registerTask('default', [
+    'newer:jshint',
+    'test',
+    'build'
+  ]);
 };
---
+++ archive/Gruntfile.js
@@ -0,0 +1,107 @@
+
+// The "wrapper" function
+module.exports = function(grunt) {
+    // Do grunt-related things in here
+    // Project configuration.
+    require('load-grunt-tasks')(grunt);
+    grunt.initConfig({
+        // imports the JSON metadata stored in package.json
+        pkg: grunt.file.readJSON('package.json'),
+        // compiles LESS file to minified CSS
+        jekyll: {
+            dev: {
+                src: 'templates',
+                dest: 'dev'
+            }
+        },
+        less: {
+            minifiedLess: {
+                options: {
+                    paths: ["./src/less", "./src/demo/less"],
+                    cleancss: true
+                },
+                files: {
+                    "./src/css/main.less-minified.css": "./src/less/bellmaker.less"
+                }
+            },
+            uncompressedLess: {
+                options: {
+                    paths: ["./src/less", "./src/demo/less"],
+                    cleancss: false
+                },
+                files: {
+                    "./src/css/main.less.css": "./src/less/bellmaker.less"
+                }
+            },
+            basicDemoLess: {
+                options: {
+                    paths: ["./src/less", "./src/demo/basic_demo/less"],
+                    cleancss: false
+                },
+                files: {
+                    "./src/demo/basic_demo/css/main.css": "./src/demo/basic_demo/less/imports.less"
+                }  
+            },
+            bootstrapDemoLess: {
+                options: {
+                    paths: ["./src/less", "./src/demo/bootstrap_demo/less"],
+                    cleancss: true
+                },
+                files: {
+                    "./src/demo/bootstrap_demo/css/main.css": "./src/demo/bootstrap_demo/less/imports.less"
+                }  
+            }
+        },
+        sass: {
+            minifiedSass: {
+                options: {
+                    style: 'compressed',
+                    precision: 4
+                },
+                files: {
+                    "./src/css/main.sass-minified.css": "./src/sass/main.scss"
+                }
+            },
+            uncompressedSass: {
+                options: {
+                    style: 'expanded',
+                    precision: 4
+                },
+                files: {
+                    "./src/css/main.sass.css": "./src/sass/main.scss"
+                }
+            }
+        },
+        watch: {
+            // runs less task when any less files change
+            less: {
+                files: ["./src/less/*", "./src/demo/*/less/*"],
+                tasks: ["less"]
+            },
+            html: {
+                files: "**/*.html"
+            },
+            sass: {
+                files: ["./src/sass/*", "./src/demo/sass/*"],
+                tasks: ['sass']  
+            }
+        }
+    });
+
+    grunt.registerTask('makeJekyll', [
+        // 'uglify'
+        'jekyll'
+    ]);
+    grunt.registerTask('default', [
+        'less:minifiedLess',
+        'less:uncompressedLess',
+        'less',
+        'sass'
+    ]);
+    grunt.registerTask('server', [
+        'watch'
+    ]);
+    grunt.registerTask('test', [
+        'less'
+    ]);
+};
